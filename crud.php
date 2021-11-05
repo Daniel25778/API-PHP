bcec5f958124811085d0694c706fd6ab4b40bb32
Daniel2577 <danielwilsonca94@gmail.com>
2021-11-05 11:01:14 -0300

    aplicando funcoes do crud

--- crud.php
+++ crud.php
@@ -7,7 +7,9 @@ include('connection.php');
 
 function read($conn){
 
-$sql = "SELECT * FROM  tbl_pessoa";
+$sql = "SELECT * FROM tbl_pessoa";
+
+// echo json_encode(array("status"=>$conn));exit;
 
 if ($resultado = mysqli_query($conn, $sql)) {
     
@@ -17,10 +19,32 @@ if ($resultado = mysqli_query($conn, $sql)) {
 
 
 }else{
-         echo json_encode(array("status"=>"success", "data"=>mysqli_error($conn)));
+         echo json_encode(array("status"=>"error", "data"=>mysqli_error($conn)));
      }
 
 }
+
+
+## FUNÇÃO DE LEITURA DE DADOS (COM CRITERIO) ##
+
+
+    function readID($cod_pessoa, $conn) {
+
+        $sql = "SELECT * FROM tbl_pessoa WHERE cod_pessoa = $cod_pessoa";
+
+        if ($resultado = mysqli_query($conn, $sql)) {
+
+            $dados = mysqli_fetch_all($resultado);
+
+            echo json_encode(array("status"=>"sucess", "data"=>$dados));
+
+        } else {
+            
+            echo json_encode(array("status"=>"error", "data"=>mysqli_error($conn)));
+        }
+    }
+
+
 
 ## FUNÇÃO DE INSERÇÃO ##
 
@@ -36,3 +60,35 @@ function create($nome, $sobrenome, $email, $celular, $fotografia, $conn){
    
 }
 
+## FUNÇÃO DE ATUALIZAÇÃO
+
+function update($cod_pessoa,$nome, $sobrenome, $email, $celular, $fotografia, $conn){
+
+    $sql = "UPDATE tbl_pessoa SET 
+    nome = '$nome',
+    sobrenome ='$sobrenome',
+    email = '$email',
+    celular = $celular,
+    fotografia = '$fotografia'
+    WHERE cod_pessoa = '$cod_pessoa'";
+
+    if (mysqli_query($conn, $sql)) {
+        echo json_encode(array("status"=>"success", "data"=>"Dados atualizados com sucesso!"));
+    } else {
+        echo json_encode(array("status"=>"error", "data"=>mysqli_error($conn)));
+    }
+    
+ }
+
+## FUNÇÃO DE EXCLUSÃO ##
+
+function deletar($cod_pessoa, $conn){
+
+     $sql = "DELETE FROM tbl_pessoa WHERE cod_pessoa = $cod_pessoa";
+
+     if (mysqli_query($conn, $sql)) {
+        echo json_encode(array("status"=>"success", "data"=>"Dados excluídos com sucesso!"));
+    } else {
+        echo json_encode(array("status"=>"error", "data"=>mysqli_error($conn)));
+    }
+}
--- index.php
+++ index.php
@@ -19,6 +19,13 @@
       read($conn);
   }
 
+
+
+  if ($acao == "readID") {
+    $cod_pessoa = $_REQUEST["cod_pessoa"];
+    readID($cod_pessoa, $conn);
+  }
+
   ## ROTAS DO CREATE ##
 
   if ($acao == 'create') {
@@ -31,3 +38,24 @@
     create($nome, $sobrenome, $email, $celular, $fotografia, $conn);
   }
   
+## ROTAS DO UPDATE
+
+  if ($acao == 'update') {
+    $cod_pessoa = $_REQUEST["cod_pessoa"];
+    $nome = $_REQUEST["nome"];
+    $sobrenome = $_REQUEST["sobrenome"];
+    $email = $_REQUEST["email"];
+    $celular = $_REQUEST["celular"];
+    $fotografia = $_REQUEST["fotografia"];
+
+    update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn);
+  }
+
+  ## ROTAS DO DELETE
+
+  if ($acao == "delete") {
+      $cod_pessoa = $_REQUEST["cod_pessoa"];
+      deletar($cod_pessoa, $conn);
+  }
+
+    
